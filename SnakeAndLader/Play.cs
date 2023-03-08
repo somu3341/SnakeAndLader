ba1a36a4d8c8fdab136f84150d3ccbaef0373ee3
somu3341 <somashekarvsv1996@gmail.com>
2023-03-08 07:39:21 +0530

    [somu]Add:Playing Game With Player2

--- SnakeAndLader/Play.cs
+++ SnakeAndLader/Play.cs
@@ -4,7 +4,8 @@ namespace SnakeAndLader
     public class Play
     {
         const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100;
-        int PlayerPosition = 0, count = 0;
+        int PlayerPosition1 = 0, count = 0;
+        int PlayerPosition2 = 0;
         Random random = new Random();
         public int DieRoll()
         {
@@ -15,31 +16,56 @@ namespace SnakeAndLader
         }
         public void GamePlay()
         {
-            while (this.PlayerPosition < WINNING_POSITION)
+            while (this.PlayerPosition1 < WINNING_POSITION)
             {
-            int option=random.Next(0, 3);
-                switch (option)
+             int option1 = random.Next(0, 3);
+             
+                switch (option1)
                 {
                     case NO_PLAY:
                         break;
                     case LADDER:
-                        this.PlayerPosition += DieRoll();                       
+                        this.PlayerPosition1 += DieRoll();                       
                         break;
                     case SNAKE:
                         int dieRoll = DieRoll();
-                        if (this.PlayerPosition - dieRoll > 0)
-                            this.PlayerPosition -= dieRoll;
+                        if (this.PlayerPosition1 - dieRoll > 0)
+                            this.PlayerPosition1 -= dieRoll;
                         else
-                            this.PlayerPosition = 0;
+                            this.PlayerPosition1 = 0;
+                        if (this.PlayerPosition1 == WINNING_POSITION)
+                        {
+                            Console.WriteLine("Player position1  " + PlayerPosition1);
+                            Console.WriteLine("DieCount---->" + count);
+                        }                       
                         break;
                 }
-                Console.WriteLine("Player position  " + PlayerPosition);
+                int option2 = random.Next(0, 3);
+                switch (option2)
+                {
+                    case NO_PLAY:
+                        break;
+                    case LADDER:
+                        this.PlayerPosition2 += DieRoll();                     
+                        if (this.PlayerPosition2 >= WINNING_POSITION)
+                        {
+                            Console.WriteLine("Player position  " + PlayerPosition2);
+                            Console.WriteLine("DieCount " + count);
+                        }
+                        break;
+                    case SNAKE:
+                        this.PlayerPosition2 += DieRoll();
+                        break;
+
+                }   
             }
-            if (this.PlayerPosition == WINNING_POSITION)
-            {
-                Console.WriteLine("Player position  " + PlayerPosition);
-                Console.WriteLine("DieCount---->" + count);
-            }
+            Console.WriteLine("Player1 position  " + PlayerPosition1);
+            Console.WriteLine("Player2 position  " + PlayerPosition2);
+            if (PlayerPosition1 < PlayerPosition2)
+                Console.WriteLine("Player2 is winner");
+            else
+                Console.WriteLine("Player1 is winner");
         }
     }
 }
+    
