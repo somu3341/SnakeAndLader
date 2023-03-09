a8fe71a90e2d5a5b7cb32c80cd30d410fc0fc40f
somu3341 <somashekarvsv1996@gmail.com>
2023-03-09 22:08:40 +0530

    [somu]Add:Game Between 2 Player.

--- SnakeAndLader/Program.cs
+++ SnakeAndLader/Program.cs
@@ -6,10 +6,13 @@ namespace SnakeAndLader
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Game");
-            //Play play = new Play();
-            //play.DieRoll();
-            Play play = new Play();
-            play.GamePlay();
+            Play play1 = new Play();
+            play1.GamePlay();
+            int Player1 = play1.GamePlay();
+            Play play2 = new Play();
+            play2.GamePlay();
+            int Player2 = play2.GamePlay();
+            play2.winner(Player1, Player2);
         }
     }
 }
